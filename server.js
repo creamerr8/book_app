4072b80587988e14f8f15977490e0131df302928
Ryan <creamerr8@gmail.com>
2020-05-21 17:55:14 -0700

    finished lab 12 started work on lab 13 still needs css

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const pg = require('pg')
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.static('./public'));
-const books = [];
 
 // config
 app.use(cors());
@@ -32,28 +31,29 @@ app.get('/searches/new', searches);
 
 app.post('/searches', getBooks);
 
-app.get('/books:id', bookdetails)
+app.get('/books/:id', bookdetails)
+
+app.put('/books/:id/update', bookUpdate)
 
 app.post('/books', saveBooks);
 
 
 
+
+
 // functionality
 function index(req, res){
-
-  //DONE: check data basse if there are any books
-  // DONE: logic check: if nothing in data base send to search for new book
-  //TODO: if is something send from database
-
   client.query('SELECT * FROM books;')
     .then(resultSQL => {
       if(resultSQL.rowCount < 1){
       res.render('pages/searches/new');
     }else{
-      // const sqlQuery = 'SELECT * FROM books'
-      res.render('pages/index', {savedBooks: books});
-    }
-  })
+      client.query('SELECT * FROM books')
+        .then( result => {
+          res.render('pages/index', {'savedBooks': result.rows});
+        })
+      }
+    })
 }
 
 function searches(req, res) {
@@ -80,23 +80,38 @@ function getBooks(req, res){
 }
 
 function saveBooks(req, res){
-  console.log(req.body);
-  const sqlQuery = 'INSERT INTO books (title, image_url, author, description, isbn) VALUES ($1, $2, $3, $4, $5)'
-  const sqlVal = [req.body.title, req.body.author, req.body.img, req.body.description, req.body.isbn];
+  const sqlQuery = 'INSERT INTO books (title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5)'
+  const sqlVal = [req.body.title, req.body.author, req.body.isbn, req.body.image, req.body.description];
   client.query(sqlQuery, sqlVal);
   res.render('pages/books/show', {'book': req.body});
-  books.push(req.body);
 }
 
 function bookdetails(req, res){
-  const sqlQuery = `SELECT * FROM books WHERE title = '${req.params.id}'`;
-  client.query(sqlQuery)
+  console.log('test')
+  const sqlQuery = `SELECT * FROM books WHERE id=$1`;
+  const sqlVal = [req.params.id];
+  console.log(sqlVal, sqlQuery)
+  client.query(sqlQuery, sqlVal)
     .then( result => {
-      res.render('pages/books/show', {newBooks: result.row})
+      console.log('test')
+      res.render('pages/books/show', {'book': result.rows[0]})
     })
     .catch(err => {
       errors(err, res);
     })
+}
+
+function bookUpdate(req, res){
+  // const sqlQuery = 'SELECT DISTINCT book_shelf FROM books'
+  // client.query(sqlQuery)
+  //   .then(result => {
+  //     res.render('books/:id/update', {'book': })
+  //   })
+  const sqlQuery = `
+    UPDATE books
+    SET title=$2, description=$3, image_url=$4, isdn=$5
+    WHERE id=$1   
+  `;
 }
 
 // constructors
@@ -105,7 +120,8 @@ function Book (bookFile){
   this.title = bookFile.title ? bookFile.title: 'no title found';
   this.author = bookFile.authors ? bookFile.authors[0]: 'no author found';
   this.description = bookFile.description ? bookFile.description: 'no description found';
-  this.isbn = bookFile.isbn;
+  this.isbn = bookFile.industryIdentifiers.indentifier ? bookFile.industryIdentifiers.indentifier: 'no isbn';
+  this.book_shelf = 'all';
 }
 
 
